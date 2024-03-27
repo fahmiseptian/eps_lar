42b646c49cc26601f61453cec177c4a35978d4b0
fahmiseptian <kherulfahmiseptian@gmail.com>
2024-03-27 10:28:31 +0700

    Logout_seller

--- eps_lar8/resources/views/seller/asset/footer.blade.php
+++ eps_lar8/resources/views/seller/asset/footer.blade.php
@@ -1,4 +1,32 @@
-
+    <script>
+        function confirmLogout(element) {
+        var logoutUrl = element.getAttribute('data-logout-url');
+        Swal.fire({
+            title: 'Anda yakin ingin logout?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Ya, logout',
+            cancelButtonText: 'Batal',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Memanggil route logout dengan metode GET
+                $.ajax({
+                    url: logoutUrl,
+                    type: 'GET', // Menggunakan metode GET
+                    success: function(response) {
+                        // Redirect ke halaman logout atau lakukan tindakan lain sesuai respons dari controller
+                        window.location.href = "/seller/logout";
+                    },
+                    error: function(xhr, status, error) {
+                        // Tangani kesalahan jika terjadi
+                        console.error(error);
+                    }
+                });
+            }
+        });
+    }
+    </script>
     <!-- FastClick -->
     <script src='{{ asset('/plugins/fastclick/fastclick.min.js') }}'></script>
     <!-- AdminLTE App -->
